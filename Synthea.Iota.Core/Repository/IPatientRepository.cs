728c2ed74de2223200a361d947eaf9c3f71714fe
Felandil <s.fuhrhop@hotmail.de>
2019-03-28 14:11:58 +0100

    add patient repository code skeleton

---
+++ Synthea.Iota.Core/Repository/IPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Synthea.Iota.Core.Repository
+{
+  using System.Collections.Generic;
+
+  using Synthea.Iota.Core.Entity;
+
+  public interface IPatientRepository
+  {
+    List<ParsedPatient> LoadPatients();
+
+    void StorePatients(List<ParsedPatient> patients);
+  }
+}
---
+++ Synthea.Iota.Core/Repository/SqlLitePatientRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Synthea.Iota.Core.Repository
+{
+  using System.Collections.Generic;
+
+  using Synthea.Iota.Core.Entity;
+
+  public class SqlLitePatientRepository : IPatientRepository
+  {
+    /// <inheritdoc />
+    public List<ParsedPatient> LoadPatients()
+    {
+      return null;
+    }
+
+    /// <inheritdoc />
+    public void StorePatients(List<ParsedPatient> patients)
+    {
+    }
+  }
+}
